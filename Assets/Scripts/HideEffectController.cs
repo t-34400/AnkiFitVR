61e15217e6c14b2edb9ba363962b245d35d64b54
t-34400 <34400mt@gmail.com>
2025-01-04 02:31:52 +0900

    Adjust paramters

--- Assets/Scripts/HideEffectController.cs
+++ Assets/Scripts/HideEffectController.cs
@@ -1,16 +1,42 @@
+#nullable enable
+
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HideEffectController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Image targetImage = default!;
+    [SerializeField] private float startHideTime = 0.8f;
+    [SerializeField] private float transitionTime = 0.2f;
+
+    [ContextMenu("Show")]
+    public void Show()
     {
-        
+        var currentColor = targetImage.color;
+        targetImage.color = new Color(currentColor.r, currentColor.g, currentColor.b, 1);
+
+        StopAllCoroutines();
+        StartCoroutine(Hide());        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        var currentColor = targetImage.color;
+        targetImage.color = new Color(currentColor.r, currentColor.g, currentColor.b, 0);
+    }
+
+    private IEnumerator Hide()
+    {
+        yield return new WaitForSeconds(startHideTime);
+
+        var t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / transitionTime;
+            var currentColor = targetImage.color;
+            targetImage.color = new Color(currentColor.r, currentColor.g, currentColor.b, 1 - t);
+            yield return null;
+        }
     }
 }
